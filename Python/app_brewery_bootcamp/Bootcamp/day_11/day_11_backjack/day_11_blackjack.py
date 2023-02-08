0e47ca700f43bee447e07c1287de6d4774e9d8b3
Mauricio Vargas Machado <mauriciovargasmachado@Mauricios-Air.phub.net.cable.rogers.com>
2023-02-08 18:58:14 -0500

    08-02-2023/2

--- Python/app_brewery_bootcamp/Bootcamp/day_11/day_11_backjack/day_11_blackjack.py
+++ Python/app_brewery_bootcamp/Bootcamp/day_11/day_11_backjack/day_11_blackjack.py
@@ -1,3 +1,4 @@
+from day_11_blackJack_art import logo
 import random
 
 def random_card():
@@ -16,6 +17,15 @@ def calculate_score(cards):
         
     return sum(cards)
 
+def compare(user_score,computer_score):
+    if user_score == computer_score:
+        print('Draw')
+    elif computer_score ==0:
+        print('You lose, The opponent has a blackjack')
+    else:
+        print('you win!!!')
+
+print(logo)
 
 user_cards = []
 computer_cards = []
@@ -25,25 +35,28 @@ for i in range(2):
     user_cards.append(random_card())
     computer_cards.append(random_card())
     
+while not is_game_over:
 
+    user_score=calculate_score(user_cards)
+    computer_score=calculate_score(computer_cards)
+    print(f'Your cards are: {user_cards}, and the computer cards are: {computer_cards}')
+    print(f'The user score is: {user_score}, and the computer score is: {computer_score}')
 
-user_score=calculate_score(user_cards)
-computer_score=calculate_score(computer_cards)
-print(f'Your cards are: {user_cards}, and the computer cards are: {computer_cards}')
-print(f'The user score is: {user_score}, and the computer score is: {computer_score}')
-print(f'Computer first card is: {computer_cards[0]}')
-
-if user_score == 0 or computer_score == 0 or user_score >21:
-    is_game_over=True
+    if user_score == 0 or computer_score == 0 or user_score >21:
+        is_game_over=True
+    else:
+        user_ask_card=input(f"Type Y to get another card, Type N to get another pass: ")
+        user_ask_card=user_ask_card.lower()
+        if user_ask_card=='y':
+            user_cards.append(random_card())
+        else:
+            is_game_over=True    
+            
+while computer_score !=0 and computer_score <17:
+    computer_cards.append(random_card)
+    computer_score = calculate_score(computer_cards)
     
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+compare(user_score,computer_score)
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 
